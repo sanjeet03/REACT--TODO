9c9f2a29a0ce4f8a7ccb98b689785a33ac8a5d23
Sanjeet03 <53861629+sanjeet03@users.noreply.github.com>
2019-08-22 12:24:20 +0530

    Add files via upload

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Hello = props => {
+    console.log(props)
+return<h1>hello{props.name}</h1>
+}
+
+/*const hello = ()  => {
+    return(
+        <div>
+        <h1> hello sanjeet</h1></div>
+    )
+}*/
+export default Hello
